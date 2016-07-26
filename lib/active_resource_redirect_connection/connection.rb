f5ab71f09100e11909cd91cd6b6b5bbba93fbee1
Tom Streller <tom.streller@sternzeit.de>
2016-07-26 10:41:34 +0200

    Add redirection limit

--- lib/active_resource_redirect_connection/connection.rb
+++ lib/active_resource_redirect_connection/connection.rb
@@ -1,10 +1,35 @@
 require 'activeresource'
 
 ActiveResource::Connection.class_eval do
+  REDIRECT_LIMIT = 10
+
+  def get(path, headers = {})
+    with_auth { request(:get, path, REDIRECT_LIMIT, build_request_headers(headers, :get, self.site.merge(path))) }
+  end
+
+  def delete(path, headers = {})
+    with_auth { request(:delete, path, REDIRECT_LIMIT, build_request_headers(headers, :delete, self.site.merge(path))) }
+  end
+
+  def patch(path, body = '', headers = {})
+    with_auth { request(:patch, path, REDIRECT_LIMIT, body.to_s, build_request_headers(headers, :patch, self.site.merge(path))) }
+  end
+
+  def put(path, body = '', headers = {})
+    with_auth { request(:put, path, REDIRECT_LIMIT, body.to_s, build_request_headers(headers, :put, self.site.merge(path))) }
+  end
+
+  def post(path, body = '', headers = {})
+    with_auth { request(:post, path, REDIRECT_LIMIT, body.to_s, build_request_headers(headers, :post, self.site.merge(path))) }
+  end
+
+  def head(path, headers = {})
+    with_auth { request(:head, path, REDIRECT_LIMIT, build_request_headers(headers, :head, self.site.merge(path))) }
+  end
 
   private
 
-  def request(method, path, *arguments)
+  def request(method, path, limit, *arguments)
     result = ActiveSupport::Notifications.instrument("request.active_resource") do |payload|
       payload[:method]      = method
       payload[:request_uri] = "#{site.scheme}://#{site.host}:#{site.port}#{path}"
@@ -18,11 +43,12 @@ ActiveResource::Connection.class_eval do
   rescue ActiveResource::Redirection => e
     response = e.response
 
+    raise(ActiveResource::Redirection.new(response)) if limit <= 0
     raise(ActiveResource::Redirection.new(response)) unless response.header['Location'].present?
 
     location = URI.parse(response.header['Location']).path
 
-    return with_auth { request(:get, location, arguments.select { |argument| argument.is_a? Hash }.last) }
+    return with_auth { request(:get, location, limit - 1, arguments.select { |argument| argument.is_a? Hash }.last) }
   end
 
 end
